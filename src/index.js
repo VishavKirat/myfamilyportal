1603a5d166a18d7842cb31024142a06c3b2d5095
Vishavkirat <vishavkiratsingh@gmail.com>
2019-06-12 23:05:35 -0400

    Awsome commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './ReStore/rootReducer'
-const store = createStore(rootReducer,applyMiddleware(thunk))
+
+const logger = createLogger({
+    collapsed : true,
+})
+const store = createStore(rootReducer,applyMiddleware(thunk, logger))
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
 
