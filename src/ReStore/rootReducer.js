11dc50a46a6840f0fa49aae14da302eff381a962
Vishavkirat <vishavkiratsingh@gmail.com>
2019-06-20 13:36:31 -0400

    awsome

--- src/ReStore/rootReducer.js
+++ src/ReStore/rootReducer.js
@@ -2,10 +2,12 @@ import ProjectReducer from './ProjectReducer';
 import authReducer from './authReducer'
 import {combineReducers} from 'redux'
 import {firestoreReducer} from 'redux-firestore'
+import {firebaseReducer} from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project: ProjectReducer,
-    firestore: firestoreReducer
+    firestore: firestoreReducer,
+    firebase: firebaseReducer
 })
 export default rootReducer;
